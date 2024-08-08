5dbded6b8fd53d27a23a487ad95c3f743ad54b9b
Masayoshi Uehara <uehara@eln.ne.jp>
2024-08-08 12:17:23 +0900

    add

--- packages/string-utils/src/index.ts
+++ packages/string-utils/src/index.ts
@@ -2,6 +2,10 @@ export function toUpperCase(str: string): string {
   return str.toUpperCase();
 }
 
+export function toLowerCase(str: string): string {
+  return str.toLowerCase();
+}
+
 import crypto = require("crypto");
 
 /*
