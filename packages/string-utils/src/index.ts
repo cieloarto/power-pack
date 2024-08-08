4f16bfd78fd4eda1526f2b26df31c5accb4ea5f6
Masayoshi Uehara <uehara@eln.ne.jp>
2024-08-08 12:09:55 +0900

    feat: add crypto

--- packages/string-utils/src/index.ts
+++ packages/string-utils/src/index.ts
@@ -1,3 +1,20 @@
 export function toUpperCase(str: string): string {
   return str.toUpperCase();
 }
+
+import crypto = require("crypto");
+
+/*
+ ログファイル用random文字列生成
+ */
+export const createRandomString = (length: number): string => {
+  const baseString = "0123456789abcdefghijklmnopqrstuvwxyz";
+  const buffer: Buffer = crypto.randomBytes(length);
+
+  return [...Array(length)].reduce((_, current, index) => {
+    return (
+      _ +
+      baseString.charAt(Math.floor((buffer[index] / 256) * baseString.length))
+    );
+  }, "");
+};
